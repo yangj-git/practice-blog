fd6b70f54f15f40841b87fa52b9b96bd80cee856
yangjun <jun.yang@shopee.com>
2022-09-13 16:51:18 +0800

    配置一级路由

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.scss'
+import Layout from './pages/Layout'
+import Login from './pages/Login'
 
 function App() {
   return (
-    <div className='App'>
-      app
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <Routes>
+          <Route path='/' element={<Layout/>}/>
+          <Route path='/login' element={<Login/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
