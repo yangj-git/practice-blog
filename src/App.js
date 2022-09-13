da3ecc9ec618e09aeff16ce1839715b243d7fed7
yangjun <jun.yang@shopee.com>
2022-09-13 17:04:06 +0800

    引入antd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.scss'
 import Layout from './pages/Layout'
 import Login from './pages/Login'
+import { Button } from 'antd';
+
+import './App.scss'
 
 function App() {
   return (
     <BrowserRouter>
+      <Button type='primary'>Test Button</Button>
       <div className='App'>
         <Routes>
           <Route path='/' element={<Layout/>}/>
