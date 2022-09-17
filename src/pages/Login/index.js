a05bed7eb831d1136debe3aaa495e3abc76f6854
yangj <test@test.org>
2022-09-17 18:15:58 +0800

    登录页表单

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,12 +1,62 @@
-import { Card } from "antd"
+import { Card, Button, Checkbox, Form, Input } from "antd"
+import { LockOutlined, UserOutlined } from '@ant-design/icons'
 import './index.scss'
 
 function Login() {
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
   return (
     <div className="login">
       <Card className="login-container">
         <div className="login-logo">某某的博客</div>
+          <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: '请输入用户名' }]}
+          >
+            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="用户名" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: '请输入密码' }]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="密码"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>记住密码</Checkbox>
+            </Form.Item>
+
+            <a className="login-form-forgot" href="">
+              忘记密码？
+            </a>
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              登录
+            </Button>
+            &nbsp;
+            没有账号？<a href="">注册</a>
+          </Form.Item>
+        </Form>
       </Card>
+      
     </div>
   )
 }
